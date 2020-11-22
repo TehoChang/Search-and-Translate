5252219cbd8c95086ff4e5d543b59c58920a3521
Teho <changteho11@gmail.com>
2020-11-22 23:31:15 +0800

    develop

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Video from './components/Video'
+import React from 'react'
+
+class App extends React.Component{
+  
+  render() {
+    return(
+    <div>
+      <Video/>
+    </div>
+    )
+  }
+}
+
+export default App
--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
+class Video extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
@@ -30,7 +30,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="ui container">
+      <div className="ui container">        
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
@@ -50,7 +50,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default Video;
 
 
 
